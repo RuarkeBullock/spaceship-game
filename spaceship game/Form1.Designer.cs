2449d53abacb2ef1819f072bce692f8a3466d745
Ruarke Bullock <Ruarke_Bullock@outlook.com>
2025-01-16 10:45:15 -0500

    a

--- spaceship game/Form1.Designer.cs
+++ spaceship game/Form1.Designer.cs
@@ -43,7 +43,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1038, 645);
+            this.DoubleBuffered = true;
             this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.Name = "Form1";
             this.Text = "Form1";
--- spaceship game/Form1.cs
+++ spaceship game/Form1.cs
@@ -12,29 +12,34 @@ namespace spaceship_game
 {
     public partial class Form1 : Form
     {
-        Rectangle hero = new Rectangle(280, 540, 40, 40);
+        // PLayer scores
+        private int scorePlayer1 = 0;
+        private int scorePlayer2 = 0;
+
+        Random randGen = new Random();
+
+        // Create players 
+        Rectangle player1 = new Rectangle(200, 270, 20, 20);
+        Rectangle player2 = new Rectangle(400, 270, 20, 20); // Moved player 2 right
+
+        // Hero speed
         int heroSpeed = 10;
 
-        //Ball variables
-        int ballSize = 10;
-        int ballSpeed = 8;
-
-        //List of balls
+        // List of balls
         List<Rectangle> balls = new List<Rectangle>();
+        List<int> ballSpeeds = new List<int>();  // ball speeds
+        List<int> ballSizes = new List<int>();   // ball sizes
 
-        int score = 0;
-        int time = 500;
-
+        // Button presses
         bool upPressed = false;
         bool downPressed = false;
         bool wPressed = false;
         bool sPressed = false;
 
-        SolidBrush greenBrush = new SolidBrush(Color.Green);
+        // Brushes
         SolidBrush whiteBrush = new SolidBrush(Color.White);
-
-        Random randGen = new Random();
-        int randValue = 0;
+        SolidBrush greenBrush = new SolidBrush(Color.Green);
+        SolidBrush blackBrush = new SolidBrush(Color.Black);
 
         public Form1()
         {
@@ -82,50 +87,85 @@ namespace spaceship_game
 
         private void gameTime_Tick(object sender, EventArgs e)
         {
+            
 
-            //move hero
-            if (upPressed == true && hero.X > 0)
+            // Move Player 1 (up/down)
+            if (upPressed && player1.Y > 0) player1.Y -= heroSpeed;
+            if (downPressed && player1.Y < this.Height - player1.Height) player1.Y += heroSpeed;
+            
+
+            // Move Player 2 (w/s) 
+            if (wPressed && player2.Y > 0) player2.Y -= heroSpeed;
+            if (sPressed && player2.Y < this.Height - player2.Height) player2.Y += heroSpeed;
+            
+
+            // Check if Player 1 reaches the top
+            if (player1.Y <= 0) { scorePlayer1++; player1.Y = this.Height - player1.Height; }
+            scorePlayer1++;
+            
+            // Check if Player 2 reaches the top
+            if (player2.Y <= 0) { scorePlayer2++; player2.Y = this.Height - player2.Height; }
+            scorePlayer2++;
+            
+
+            // Create balls randomly
+            if (randGen.Next(0, 101) < 15)  // Reduced frequency of ball creation
             {
-                hero.X -= heroSpeed;
+                int y = randGen.Next(0, this.Height - 200);
+                int size = randGen.Next(10, 30);  // Random size 
+                int speed = randGen.Next(5, 10);  // Random speed 
+                balls.Add(new Rectangle(0, y, size, size));
+                ballSpeeds.Add(speed);
+                ballSizes.Add(size);
             }
-
-            if (downPressed == true && hero.X < this.Width - hero.Width)
-            {
-                hero.X += heroSpeed;
-            }
-
-
-            //create ball
-            randValue = randGen.Next(0, 101);
-
-            if (randValue < 50)
+            else if (randGen.Next(0, 101) < 15)
             {
                 int y = randGen.Next(0, this.Height);
-                Rectangle newBall = new Rectangle(0, y, ballSize, ballSize);
-                balls.Add(newBall);
+                int size = randGen.Next(10, 30);  // Random size 
+                int speed = randGen.Next(5, 10);  // Random speed 
+                balls.Add(new Rectangle(this.Width, y, size, size));  // Ball starts from right
+                ballSpeeds.Add(-speed);
+                ballSizes.Add(size);
             }
 
-            //check for points
+            // Move balls left or right
             for (int i = 0; i < balls.Count; i++)
             {
-                int x = balls[i].X + ballSpeed;
-                balls[i] = new Rectangle(x, balls[i].Y, ballSize, ballSize);
+                balls[i] = new Rectangle(balls[i].X + ballSpeeds[i], balls[i].Y, ballSizes[i], ballSizes[i]);
 
+                // Change direction when ball hits sides
+                if (balls[i].X <= 0 || balls[i].X >= this.Width - balls[i].Width)
+                    ballSpeeds[i] = -ballSpeeds[i];
             }
 
-            //move ball
+            // Check if Player 1 or Player 2 intersects with balls
             for (int i = 0; i < balls.Count; i++)
             {
-                if (hero.IntersectsWith(balls[i]))
+                if (player1.IntersectsWith(balls[i]))
                 {
-                    score += 10;
+
                     balls.RemoveAt(i);
+                    ballSpeeds.RemoveAt(i);
+                    ballSizes.RemoveAt(i);
+                    
+                    player1.Y = this.Height - player1.Height - 50;  // Reset Player 1 to bottom
+                }
+
+                if (player2.IntersectsWith(balls[i]))
+                {
+
+                    balls.RemoveAt(i);
+                    ballSpeeds.RemoveAt(i);
+                    ballSizes.RemoveAt(i);
+                    
+                    player2.Y = this.Height - player2.Height - 50;  // Reset Player 2 to bottom
                 }
             }
 
-            //redraw the screen
+            // Refresh the screen
             Refresh();
         }
+
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
@@ -135,8 +175,11 @@ namespace spaceship_game
                 e.Graphics.FillEllipse(greenBrush, balls[i]);
             }
 
-            //draw hero
-            e.Graphics.FillRectangle(whiteBrush, hero);
+            //draw hero1
+            e.Graphics.FillRectangle(whiteBrush, player1);
+
+            //draw hero2
+            e.Graphics.FillRectangle(whiteBrush, player2);
         }
     }
 }
